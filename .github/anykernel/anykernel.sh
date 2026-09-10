e650e65953daccf32cb9de736b5ee429bffc54b8
Rene Ortez <88523861+r3n3o@users.noreply.github.com>
2026-09-09 22:39:30 -0600

    fix(anykernel): define uppercase BLOCK and IS_SLOT_DEVICE required by AnyKernel3 v3

--- .github/anykernel/anykernel.sh
+++ .github/anykernel/anykernel.sh
@@ -1,11 +1,15 @@
-# AnyKernel3 Config para Miatoll (Qualcomm SM6250)
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
 properties() { '
 kernel.string=Miatoll NetHunter Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuntil=500
+do.cleanuponabort=0
 device.name1=miatoll
 device.name2=curtana
 device.name3=joyeuse
@@ -13,14 +17,22 @@ device.name4=gram
 device.name5=excalibur
 supported.versions=
 supported.patchlevels=
-'; }
+supported.vendorpatchlevels=
+'; } # end properties
 
-# Enlace universal por nombre gestionado por Android/TWRP
-block=/dev/block/bootdevice/by-name/boot;
+### AnyKernel install
+# boot shell variables (AK3 standard uppercase + fallback)
+BLOCK=boot;
+block=boot;
+IS_SLOT_DEVICE=0;
 is_slot_device=0;
-ramdisk_build_dts=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
+# import functions/variables and setup patching (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+# boot install
 dump_boot;
 write_boot;
+## end boot install
